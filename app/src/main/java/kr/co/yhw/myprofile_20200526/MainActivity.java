df3ca798219d567392b5f0613a0fb11cffdce4dd
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-26 17:47:47 +0900

    Glide로 이미지 띄우기

--- app/src/main/java/kr/co/yhw/myprofile_20200526/MainActivity.java
+++ app/src/main/java/kr/co/yhw/myprofile_20200526/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
+
 import kr.co.yhw.myprofile_20200526.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
@@ -26,6 +28,8 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+//        인터넷에 있는 이미지 불러오기 => 인터넷 연결 권한 필요
+        Glide.with(mContext).load("https://pbs.twimg.com/profile_images/1166183517060943872/UJlK2ff4_400x400.jpg").into(binding.profileImg);
 
     }
 }
