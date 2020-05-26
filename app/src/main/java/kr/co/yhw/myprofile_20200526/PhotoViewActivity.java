7264d2a1ffeb79308d997366dab0b9ae8e5dfa60
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-26 17:58:33 +0900

    PhotoView로 사진 크게 보기   - src를 이용해 직접 사진배치

---
+++ app/src/main/java/kr/co/yhw/myprofile_20200526/PhotoViewActivity.java
@@ -0,0 +1,31 @@
+package kr.co.yhw.myprofile_20200526;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import kr.co.yhw.myprofile_20200526.databinding.ActivityPhotoViewBinding;
+
+public class PhotoViewActivity extends BaseActivity {
+
+    ActivityPhotoViewBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_photo_view);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
